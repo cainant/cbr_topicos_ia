43812ac44d35a55b5b1cb8fa2932691c31fa28bf
ctravizanutto <ctravizanutto@gmail.com>
2024-12-09 19:42:15 -0300

    menu subclass

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 import pandas as pd
 
 class CBR():
+    class _Menu():
+        def __init__(self, menu: pd.DataFrame):
+            energy, carb, protein, fat, fiber, vitC, vitD, vitE = 0, 0, 0, 0, 0, 0, 0, 0
+            for _, value in menu.iterrows():
+                energy += value['energia2_media']
+                carb += value['carboidrato_disponivel_media']
+                protein += value['proteina_media']
+                fat += value['lipidios_media']
+                fiber += value['fibra_alimentar_media']
+                vitC += value['vitamina_C_media']
+                vitD += value['vitamina_D_media']
+                vitE += value['vitamina_E_media']
+
+            self.menu = menu
+            self.energy = energy
+            self.nutrient_table = {'carb': carb, 'protein': protein, 'fat': fat, 'fiber': fiber, 'vitC': vitC, 'vitD': vitD, 'vitE': vitE}
+
+        def __repr__(self):
+            nutrient_table = f'''
+Tabela Nutricional:\n---------------------------
+Energia: {self.energy} kcal
+Carboidratos: {self.nutrient_table['carb']} g
+Proteinas: {self.nutrient_table['protein']} g
+Lipidios: {self.nutrient_table['fat']} g
+Fibras: {self.nutrient_table['fiber']} g
+Vitamina C: {self.nutrient_table['vitC']} g
+Vitamina D: {self.nutrient_table['vitD']} g
+Vitamina E: {self.nutrient_table['vitE']} g\n
+Alimentos:\n---------------------------
+'''
+            foods = ''
+            index = 0
+            for _, value in self.menu.iterrows():
+                if index < 5:
+                    foods += f'Alimento {index + 1}: {self.menu.iloc[index]["nome_pt_br"]}\n'
+                else:
+                    foods += f'Alimento Adicionado {index + 1}: {self.menu.iloc[index]["nome_pt_br"]}\n'
+                index += 1
+            return nutrient_table + foods
+
     def __init__(self):
-        self._load_data()
-        self._generate_case_library()
+        self.foods = self._load_data()
+        self.case_library = [self._generate_case(self.foods) for _ in range(10)]
+        print(self.case_library[0])
 
     def _load_data(self):
         foods = pd.read_excel('alimentos.xlsx')
@@ -15,19 +56,18 @@ class CBR():
         foods = foods.merge(groups, left_on='grupo_id', right_on='id').drop('id', axis='columns')
         foods = foods.merge(nutrients, on='cod_alimento')
 
-        self.foods = foods
+        return foods
 
-    def _generate_case_library(self):
-        def _generate_case(foods: pd.DataFrame):
-            protein = foods[foods["nm_grupo_br"].isin(['Carnes e derivados', 'Pescados e frutos do mar', 'Ovos e derivados'])].sample(1)
-            vegetables = foods[foods["nm_grupo_br"].isin(['Vegetais e derivados', 'Leguminosas e derivados'])].sample(2)
-            carb  = foods[foods["nm_grupo_br"].isin(['Cereais e derivados'])].sample(1)
-            drink = foods[foods["nm_grupo_br"] == 'Bebidas '].sample(1)
+    def _generate_case(self, foods: pd.DataFrame):
+        protein = foods[foods["nm_grupo_br"].isin(['Carnes e derivados', 'Pescados e frutos do mar', 'Ovos e derivados'])].sample(1)
+        vegetables = foods[foods["nm_grupo_br"].isin(['Vegetais e derivados', 'Leguminosas e derivados'])].sample(2)
+        carb  = foods[foods["nm_grupo_br"].isin(['Cereais e derivados'])].sample(1)
+        drink = foods[foods["nm_grupo_br"] == 'Bebidas '].sample(1)
 
-            return pd.concat([protein, vegetables, carb, drink])
+        menu = pd.concat([protein, vegetables, carb, drink])
         
-        self.case_library = [_generate_case(self.foods) for _ in range(10)]
-
+        return self._Menu(menu)
+         
 
 if __name__ == '__main__':
     cbr = CBR()
