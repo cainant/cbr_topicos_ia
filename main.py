7e124180ec8b1accda51475b52f8ae810ed701d8
Caio Rufino <caiofurlanrufinosilva@gmail.com>
2024-12-15 21:39:56 -0300

    Output change

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Menu:
             f"---------------------------\n"
             f"Energia: {self.energy} kcal\n"
             f"Carboidratos: {self.nutrient_table['carb']} g\n"
-            f"Proteinas: {self.nutrient_table['protein']} g\n"
-            f"Lipidios: {self.nutrient_table['fat']} g\n"
+            f"Proteínas: {self.nutrient_table['protein']} g\n"
+            f"Lipídios: {self.nutrient_table['fat']} g\n"
             f"Fibras: {self.nutrient_table['fiber']} g\n"
             f"Vitamina C: {self.nutrient_table['vitC']} g\n"
             f"Vitamina D: {self.nutrient_table['vitD']} g\n"
@@ -23,9 +23,9 @@ class Menu:
         )
         foods = ''
         for index, _ in enumerate(self.menu.iterrows()):
-                foods += f'Alimento {index + 1}: {self.menu.iloc[index]["nome_pt_br"]} | {self.menu.iloc[index]["nm_grupo_br"]}\n'
+            foods += f'Alimento {index + 1}: {self.menu.iloc[index]["nome_pt_br"]} | {self.menu.iloc[index]["nm_grupo_br"]}\n'
         return nutrient_table + foods
-    
+
     def generate_nutrient_table(self):
         menu = self.menu
         self.energy = menu['energia2_media'].sum()
@@ -63,9 +63,8 @@ class CBR():
         drink = foods[foods["nm_grupo_br"] == 'Bebidas '].sample(1)
 
         menu = pd.concat([protein, vegetables, carb, drink])
-        
         return Menu(menu)
-         
+
     def retrieve(self, energy, threshold):
         filtered_menus = [m for m in self.case_library if energy - threshold <= m.energy <= energy + threshold]
 
@@ -85,6 +84,7 @@ class CBR():
         new_food = self.foods[(self.foods['grupo_id'] == to_change_group) & (~self.foods['cod_alimento'].isin(menu.menu['cod_alimento']))].sample(1)
 
         if not new_food.empty:
+            print(f"Substituindo: {to_change['nome_pt_br']} ({to_change['nm_grupo_br']}) por {new_food.iloc[0]['nome_pt_br']} ({new_food.iloc[0]['nm_grupo_br']})")
             menu.menu = menu.menu[menu.menu['cod_alimento'] != to_change['cod_alimento']]
             menu.menu = pd.concat([menu.menu, new_food])
 
@@ -96,13 +96,13 @@ class CBR():
     def revise(self, menu: Menu, energy, threshold):
         menu.generate_nutrient_table()
 
-        if (menu.energy <= energy + threshold or menu.energy >= energy - threshold):
+        if (energy - threshold <= menu.energy <= energy + threshold):
             print(f'NOVO MENU\n{menu}')
             self.retain(menu)
             return menu
         else:
             return None
-        
+
     def retain(self, menu):
         self.case_library.append(menu)
 
@@ -111,3 +111,4 @@ if __name__ == '__main__':
     energia1_med = int(input("Digite a energia média: "))
     limiar = int(input("Digite o limiar de energia: "))
     menu = cbr.retrieve(energia1_med, limiar)
+    
